21171648bc3399ce9f8431d0f1bb0f14418c92d6
BrianFs04 <brayanflorezsanabria@gmail.com>
2021-06-22 16:15:21 -0500

    [Feature]: String literal types

--- 0x00-TypeScript/task_2/js/main.ts
+++ 0x00-TypeScript/task_2/js/main.ts
@@ -57,3 +57,14 @@ export function executeWork(employee: DirectorInterface | TeacherInterface): str
 	}
 	return employee.workTeacherTasks();
 }
+
+type Subjects = 'Math' | 'History';
+
+export function teachClass(todayClass: Subjects): string {
+    if (todayClass === 'Math') {
+        return 'Teaching Math';
+    }
+    if (todayClass === 'History') {
+        return 'Teaching History'
+    }
+}
