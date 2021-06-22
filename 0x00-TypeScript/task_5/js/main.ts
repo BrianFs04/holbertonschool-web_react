0216d3722c9cc54548e3bac326b1804a86d10ec9
BrianFs04 <brayanflorezsanabria@gmail.com>
2021-06-22 17:24:49 -0500

    [Fix]

--- 0x00-TypeScript/task_5/js/main.ts
+++ 0x00-TypeScript/task_5/js/main.ts
@@ -8,10 +8,10 @@ export interface MinorCredits {
     _brand: 'minor',
 }
 
-export const sumMajorCredits = (subject1: MajorCredits, subject2: MinorCredits) => {
+export function sumMajorCredits(subject1: MajorCredits, subject2: MajorCredits) {
     return { credits: subject1.credits + subject2.credits } as MajorCredits;
 }
 
-export const sumMinorCredits = (subject1: MinorCredits, subject2: MajorCredits) => {
+export function sumMinorCredits(subject1: MinorCredits, subject2: MinorCredits) {
     return { credits: subject1.credits + subject2.credits } as MinorCredits;
 }
