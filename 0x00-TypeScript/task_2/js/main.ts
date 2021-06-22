264e0b204cd697a2e804292686926966b45c8da0
BrianFs04 <brayanflorezsanabria@gmail.com>
2021-06-22 16:06:00 -0500

    [Fix]: Format

--- 0x00-TypeScript/task_2/js/main.ts
+++ 0x00-TypeScript/task_2/js/main.ts
@@ -1,57 +1,63 @@
 interface DirectorInterface {
-    workFromHome(): string,
-    getCoffeeBreak(): string,
-    workDirectorTasks(): string
+	workFromHome(): string;
+	getCoffeeBreak(): string;
+	workDirectorTasks(): string;
 }
 
 interface TeacherInterface {
-    workFromHome(): string,
-    getCoffeeBreak(): string,
-    workTeacherTasks(): string
+	workFromHome(): string;
+	getCoffeeBreak(): string;
+	workTeacherTasks(): string;
 }
 
 export class Director implements DirectorInterface {
-    workFromHome(): string {
-        return 'Working from home';
-    }
+	workFromHome(): string {
+		return 'Working from home';
+	}
 
-    getCoffeeBreak(): string {
-        return 'Getting a coffee break';
-    }
+	getCoffeeBreak(): string {
+		return 'Getting a coffee break';
+	}
 
-    workDirectorTasks(): string {
-        return 'Getting to director tasks';
-    }
+	workDirectorTasks(): string {
+		return 'Getting to director tasks';
+	}
 }
 
 export class Teacher implements TeacherInterface {
-    workFromHome(): string {
-        return 'Cannot work from home';
-    }
+	workFromHome(): string {
+		return 'Cannot work from home';
+	}
 
-    getCoffeeBreak(): string {
-        return 'Cannot have a break';
-    }
+	getCoffeeBreak(): string {
+		return 'Cannot have a break';
+	}
 
-    workTeacherTasks(): string {
-        return 'Getting to work';
-    }
+	workTeacherTasks(): string {
+		return 'Getting to work';
+	}
 }
 
-export const createEmployee = (salary: number | string): DirectorInterface | TeacherInterface => {
-    if (typeof salary === 'number' && salary < 500) {
-        return new Teacher();
-    }
-    return new Director();
-}
+export const createEmployee = (
+	salary: number | string
+): DirectorInterface | TeacherInterface => {
+	if (typeof salary === 'number' && salary < 500) {
+		return new Teacher();
+	}
+	return new Director();
+};
 
-export const isDirector = (employee: DirectorInterface | TeacherInterface): employee is Director => {
-    return employee.workFromHome() === 'Working from home';
-}
+export const isDirector = (
+	employee: DirectorInterface | TeacherInterface
+): employee is Director => {
+	return employee.workFromHome() === 'Working from home';
+};
 
-export function executeWork(employee: DirectorInterface | TeacherInterface): string {
-    if (isDirector(employee)) {
-        return employee.workDirectorTasks();
-    }
-    return employee.workTeacherTasks();
+export function executeWork(
+	employee: DirectorInterface | TeacherInterface
+): string {
+	if (isDirector(employee)) {
+		return employee.workDirectorTasks();
+	}
+	return employee.workTeacherTasks();
 }
