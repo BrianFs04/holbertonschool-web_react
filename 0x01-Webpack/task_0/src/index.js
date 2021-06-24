686c2de2a84284c39155133a3b4bd0e82ceaccff
BrianFs04 <brayanflorezsanabria@gmail.com>
2021-06-24 13:30:38 -0500

    [Delete]: jQuery when a document is loaded

--- 0x01-Webpack/task_0/src/index.js
+++ 0x01-Webpack/task_0/src/index.js
@@ -1,9 +1,7 @@
 import $ from 'jquery';
 
-$(function () {
-	$('body').prepend(
-		$('<p>').text('Holberton Dashboard'),
-		$('<p>').text('Dashboard data for the students'),
-		$('<p>').text('Copyright - Holberton School')
-	);
-});
+$('body').prepend(
+	$('<p>').text('Holberton Dashboard'),
+	$('<p>').text('Dashboard data for the students'),
+	$('<p>').text('Copyright - Holberton School')
+);
