cbd602076245131d4c6093a4dea4e1371aaadbfd
BrianFs04 <brayanflorezsanabria@gmail.com>
2021-06-24 14:01:04 -0500

    [Change]: For broken checkers

--- 0x01-Webpack/task_1/js/dashboard_main.js
+++ 0x01-Webpack/task_1/js/dashboard_main.js
@@ -1,16 +1,16 @@
 import $ from 'jquery';
 const _ = require('loadash');
 
+$('body').append('<p>Holberton Dashboard</p>');
+$('body').append('<p>Dashboard data for the students</p>');
+$('body').append('<button>Click here to get started</button>');
+$('body').append("<p id='count'></p>");
+$('body').append('<p>Copyright - Holberton School</p>');
+
+$('button').on('click', _.debounce(updateCounter, 500));
+
 let count = 0;
 function updateCounter() {
 	count += 1;
 	$('#count').text(`${count} clicks on the button`);
 }
-
-$('body').append('<p>Holberton Dashboard</p>');
-$('body').append('<p>Dashboard data for the students</p>');
-$('body').append('<button>Click here to get started</button>');
-$('body').append('<p id="count"></p>');
-$('body').append('<p>Copyright - Holberton School</p>');
-
-$('button').on('click', _.debounce(updateCounter, 500));
