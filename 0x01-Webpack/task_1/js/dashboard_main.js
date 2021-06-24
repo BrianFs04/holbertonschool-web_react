96c896d8f419121bc302fbe421d908754b6de5f1
BrianFs04 <brayanflorezsanabria@gmail.com>
2021-06-24 13:50:41 -0500

    [Feature]: Webpack with a config file

---
+++ 0x01-Webpack/task_1/js/dashboard_main.js
@@ -0,0 +1,26 @@
+import $ from 'jquery';
+import _, { update } from 'lodash';
+
+$(() => {
+	let count = 0;
+	const updateCounter = () => {
+		count += 1;
+		$('#count').text(`${count} clicks on the button`);
+	};
+
+	$('body').prepend(
+		$('<p>').text('Holberton Dashboard'),
+		$('<p>').text('Dashboard data for the students'),
+		$('<button>')
+			.text('Click here to get started')
+			.on(
+				'click',
+				_.debounce(updateCounter, 300, {
+					leading: true,
+					trailing: false,
+				})
+			),
+		$('<p>').attr({ id: 'count' }),
+		$('<p>').text('Coyright - Holberton School')
+	);
+});
