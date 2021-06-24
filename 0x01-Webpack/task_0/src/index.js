9176aad0384bb82dd1a35360223f663bededa1a4
BrianFs04 <brayanflorezsanabria@gmail.com>
2021-06-24 13:34:19 -0500

    [Change]: prepend to append

--- 0x01-Webpack/task_0/src/index.js
+++ 0x01-Webpack/task_0/src/index.js
@@ -1,6 +1,6 @@
 import $ from 'jquery';
 
-$('body').prepend(
+$('body').append(
 	$('<p>').text('Holberton Dashboard'),
 	$('<p>').text('Dashboard data for the students'),
 	$('<p>').text('Copyright - Holberton School')
