e9e12bfd57a31fb825baff45534452e19b0431f6
BrianFs04 <brayanflorezsanabria@gmail.com>
2021-06-30 11:52:00 -0500

    [Feature]: Deploy to a GitHub page

---
+++ 0x02-react_intro/task_4/src/App.js
@@ -0,0 +1,33 @@
+import logo from './holberton-logo.jpg';
+import { getFullYear, getFooterCopy } from './utils';
+import './App.css';
+
+function App() {
+	return (
+		<>
+			<header className='App-header'>
+				<img width='150px' height='150px' src={logo} alt='logo'></img>
+				<h1>School dashboard</h1>
+			</header>
+			<main className='App-body'>
+				<p>Login to access the full dashboard</p>
+				<label>
+					Email:
+					<input type='email'></input>
+				</label>
+				<label>
+					Password:
+					<input type='password'></input>
+				</label>
+				<button type='button'>OK</button>
+			</main>
+			<footer className='App-footer'>
+				<p>
+					Copyright {getFullYear()} - {getFooterCopy(true)}
+				</p>
+			</footer>
+		</>
+	);
+}
+
+export default App;
---
+++ 0x02-react_intro/task_4/src/Notifications.test.js
@@ -0,0 +1,17 @@
+import { shallow } from 'enzyme';
+import { expect } from 'chai';
+import Notifications from './Notifications';
+
+const wrapper = shallow(<Notifications />);
+
+it('test that Notifications renders without crashing', () => {
+	shallow(<Notifications />);
+});
+
+it('verify that Notifications renders three list items', () => {
+	expect(wrapper.find('ul').children().length).equal(3);
+});
+
+it('verify that Notifications renders the text Here is the list of notifications', () => {
+	expect(wrapper.find('p').text()).equal('Here is the list of notifications');
+});
