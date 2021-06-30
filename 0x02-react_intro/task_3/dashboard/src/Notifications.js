2e5710de4e0f41acfe3654b42acc7d77facde80b
BrianFs04 <brayanflorezsanabria@gmail.com>
2021-06-30 11:51:35 -0500

    [Feature]: Adding some tests

---
+++ 0x02-react_intro/task_3/dashboard/src/Notifications.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import closeIcon from './close-icon.png';
+import { getLatestNotification } from './utils';
+import './Notifications.css';
+
+const Notifications = () => {
+	return (
+		<div className='Notifications'>
+			<p>Here is the list of notifications</p>
+			<ul>
+				<li data-priority='default'>New course available</li>
+				<li data-priority='urgent'>New resume available</li>
+				<li
+					data-priority='urgent'
+					dangerouslySetInnerHTML={{
+						__html: `${getLatestNotification()}`,
+					}}
+				></li>
+			</ul>
+			<button
+				type='button'
+				style={{
+					position: 'absolute',
+					top: '11px',
+					right: '10px',
+					background: 'none',
+					border: 'none',
+					cursor: 'pointer',
+				}}
+				aria-label='Close'
+				onClick={() => console.log('Close button has been clicked')}
+			>
+				<img width='9px' src={closeIcon} alt='Close icon'></img>
+			</button>
+		</div>
+	);
+};
+
+export default Notifications;
