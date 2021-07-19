5cee4463152e7d821ec6373726dc5dd512fab38b
BrianFs04 <brayanflorezsanabria@gmail.com>
2021-07-18 19:03:46 -0500

    [Feature]: Handling events

---
+++ 0x04-React_component/task_2/dashboard/src/App/App.test.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import { expect } from 'chai';
+import App from './App';
+
+const wrapper = shallow(<App />);
+
+it('test that App renders without crashing', () => {
+	shallow(<App />);
+});
+
+it('It should contain the Notifications component', () => {
+	expect(wrapper.find('Notifications').exists()).equal(true);
+});
+
+it('It should contain the Header component', () => {
+	expect(wrapper.find('Header').exists()).equal(true);
+});
+
+it('It should contain the Login component', () => {
+	expect(wrapper.find('Login').exists()).equal(true);
+});
+
+it('It should contain the Footer component', () => {
+	expect(wrapper.find('Footer').exists()).equal(true);
+});
+
+it('CourseList not displayed - when is not logged in', () => {
+	expect(wrapper.find('CourseList').exists()).equal(false);
+});
+
+it('When is logged in', () => {
+	const wrapper = shallow(<App isLoggedIn={true} />);
+	expect(wrapper.find('Login').exists()).equal(false);
+	expect(wrapper.find('CourseList').exists()).equal(true);
+});
+
+it('When ctrl+h key are pressed down', () => {
+	const logOut = jest.fn(() => null);
+	const alert = jest.spyOn(window, 'alert');
+	expect(alert);
+	expect(logOut);
+	jest.restoreAllMocks();
+});
---
+++ 0x04-React_component/task_2/dashboard/src/Notifications/NotificationItem.test.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import NotificationItem from './NotificationItem';
+
+const wrapper = shallow(<NotificationItem />);
+
+it('test that NotificationItem renders without crashing', () => {
+	shallow(<NotificationItem />);
+});
+
+it('renders the correct html for type and value props', () => {
+	const wrapper = shallow(<NotificationItem type='default' value='test' />);
+	const li = wrapper.find('li');
+	expect(li.exists()).toEqual(true);
+	expect(li.text()).toEqual('test');
+	expect(li.prop('data-notification-type')).toEqual('default');
+});
+
+it('renders the correct html for html prop', () => {
+	const text = 'Here is the list of notifications';
+	const wrapper = shallow(
+		<NotificationItem html={{ __html: '<u>test</u>' }} />
+	);
+	const li = wrapper.find('li');
+	expect(li.exists()).toEqual(true);
+	expect(li.html()).toEqual(
+		'<li data-notification-type="default"><u>test</u></li>'
+	);
+});
+
+describe('markAsRead', () => {
+	const wrapper = shallow(
+		<NotificationItem type='default' value='test' id={1} />
+	);
+	it('spy on it', () => {
+		const instance = wrapper.instance();
+		instance.markAsRead = jest.fn();
+		const firstItem = wrapper.find('li').first();
+		firstItem.simulate('click');
+		instance.markAsRead(1);
+		expect(instance.markAsRead).toHaveBeenCalledWith(1);
+		jest.restoreAllMocks();
+	});
+});
